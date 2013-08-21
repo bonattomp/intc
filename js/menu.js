750009144cffcea9304ca7a518918589702e6239
unknown <mbonatto@MBonatto_DT.main.inc>
2013-08-21 17:04:53 -0400

    First commit

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+document.write('<ul>');
+ 
+document.write('<a href="#" class="nav_text_aling">New Arrivals</a>  | ');
+ 
+document.write('<a href="#" class="nav_text_aling">Boys</a>  | ');
+ 
+document.write('<a href="#" class="nav_text_aling">Coupons</a>  | ');
+ 
+document.write('<a href="#" class="nav_text_aling">Location</a>  | ');
+ 
+document.write('<a href="#" class="nav_text_aling">About Us</a>  | ');
+ 
+document.write('<a href="#" class="nav_text_aling">Careers</a>  | ');
+ 
+document.write('<a href="#" class="nav_text_aling">Contact</a>  ');
+
+document.write('</li>');
+ 
+document.write('</ul>');
