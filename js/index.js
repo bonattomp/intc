7d01c713fc19fa069f3c409235ecccbaaebd6ee7
bonattomp <bonattomp@gmail.com>
2013-08-29 18:34:27 -0400

    menu / footer / locations

--- js/index.js
+++ js/index.js
@@ -3,3 +3,21 @@ $(document).ready(function() {
 		$("#menu").html(data);
 	});
 }); 
+
+$(document).ready(function() {
+	$.get("banner.html", function(data) {
+		$("#banner").html(data);
+	});
+}); 
+
+$(document).ready(function() {
+	$.get("mini_boxes.html", function(data) {
+		$("#mini_boxes").html(data);
+	});
+}); 
+
+$(document).ready(function() {
+	$.get("location.html", function(data) {
+		$("#location").html(data);
+	});
+}); 
