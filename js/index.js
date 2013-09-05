ec45485833168bb9294073a8dddb525fa25d8649
bonattomp <bonattomp@gmail.com>
2013-09-05 09:28:51 -0400

    new arrivals page coding
    - new arrivals.html
    - new arrivals thumbnails.html
    - internal.js

--- js/index.js
+++ js/index.js
@@ -1,35 +1,37 @@
 $(document).ready(function() {
-	$.get("menu.html", function(data) {
+	$.get("../../inc/menu.html", function(data) {
 		$("#menu").html(data);
 	});
 }); 
 
 $(document).ready(function() {
-	$.get("banner.html", function(data) {
+	$.get("../../inc/banner.html", function(data) {
 		$("#banner").html(data);
 	});
 }); 
 
 $(document).ready(function() {
-	$.get("mini_boxes.html", function(data) {
+	$.get("../../inc/mini_boxes.html", function(data) {
 		$("#mini_boxes").html(data);
 	});
 }); 
 
 $(document).ready(function() {
-	$.get("location.html", function(data) {
+	$.get("../../inc/location.html", function(data) {
 		$("#location").html(data);
 	});
 }); 
 
 $(document).ready(function() {
-	$.get("footer.html", function(data) {
+	$.get("../../inc/footer.html", function(data) {
 		$("#footer").html(data);
 	});
 }); 
 $(document).ready(function() {
-	$.get("coupons.html", function(data) {
+	$.get("../../inc/coupons.html", function(data) {
 		$("#coupons").html(data);
 	});
 }); 
+ 
 
+ 
