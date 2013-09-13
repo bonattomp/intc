309f7e414f12216bd90d5d2f2e327f20196ae27f
bonattomp <bonattomp@gmail.com>
2013-09-13 17:43:26 -0400

    IE fixing menu

--- js/index.js
+++ js/index.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
-	$.get("../../inc/menu.html", function(data) {
+	$.get("menu.html", function(data) {
 		$("#menu").html(data);
 	});
 }); 
 
 $(document).ready(function() {
-	$.get("../../inc/banner.html", function(data) {
+	$.get("banner.html", function(data) {
 		$("#banner").html(data);
 	});
 }); 
 
 $(document).ready(function() {
-	$.get("../../inc/mini_boxes.html", function(data) {
+	$.get("mini_boxes.html", function(data) {
 		$("#mini_boxes").html(data);
 	});
 }); 
 
 $(document).ready(function() {
-	$.get("../../inc/location.html", function(data) {
+	$.get("location.html", function(data) {
 		$("#location").html(data);
 	});
 }); 
 
 $(document).ready(function() {
-	$.get("../../inc/footer.html", function(data) {
+	$.get("footer.html", function(data) {
 		$("#footer").html(data);
 	});
 }); 
 $(document).ready(function() {
-	$.get("../../inc/coupons_thumbnails.html", function(data) {
+	$.get("coupons_thumbnails.html", function(data) {
 		$("#coupons").html(data);
 	});
 }); 
