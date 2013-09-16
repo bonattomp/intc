57fbf971f017f3937f4444b549be7852f0264cad
bonattomp <bonattomp@gmail.com>
2013-09-16 15:50:05 -0400

    comitando por comitá

--- js/internal.js
+++ js/internal.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$.get("new_arrivals_thumbnails.html", function(data) {
+	$.get("../../inc/new_arrivals_thumbnails.html", function(data) {
 		$("#new_arrivals_thumbnails").html(data);
 	});
 }); 
