fe9135df9ffb544ea90b8d461be91f0924242938
bonattomp <bonattomp@gmail.com>
2013-09-17 10:45:18 -0400

    IE menu
    From this point all internal pages are fixed - Index still not fixed

--- js/internal.js
+++ js/internal.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$.get("../../inc/new_arrivals_thumbnails.html", function(data) {
+	$.get("new_arrivals_thumbnails.html", function(data) {
 		$("#new_arrivals_thumbnails").html(data);
 	});
 }); 
