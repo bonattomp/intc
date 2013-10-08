5927465ee85b724dcbafb6ab34fba41d2ffb92b7
bonattomp <bonattomp@gmail.com>
2013-10-08 17:54:55 -0400

    Mens dropdown option

--- js/internal.js
+++ js/internal.js
@@ -1,20 +1,21 @@
- // all productos sections from the intenal pages
+ // all products sections from the intenal pages
  
  
-// Boys submenu
+// CATEGORY BOYS
 
 $(document).ready(function() {
 	$.get("../../category/boys_submenu.html", function(data) {
 		$("#boys_submenu").html(data);
 	});
 }); 
+	// call submenu for smartphone format
 $(document).ready(function() {
 	$.get("../../category/boys_submenu.html", function(data) {
 		$("#boys_submenu_xs").html(data);
 	});
 }); 
 
-// Boys sections
+// Boys thumbnais
 
 $(document).ready(function() {
 	$.get("../../category/boys/boys_tops_thumbnails.html", function(data) {
@@ -32,16 +33,44 @@ $(document).ready(function() {
 	});
 }); 
 $(document).ready(function() {
-	$.get("../../category/boys/boys_suits_thumbnails.html", function(data) {
-		$("#boys_suits_thumbnails").html(data);
+	$.get("../../category/boys/boys_dress_thumbnails.html", function(data) {
+		$("#boys_dress_thumbnails").html(data);
+	});
+}); 
+
+// CATEGORY MENS
+
+$(document).ready(function() {
+	$.get("../../category/mens_submenu.html", function(data) {
+		$("#mens_submenu").html(data);
+	});
+}); 
+	// call submenu for smartphone format
+$(document).ready(function() {
+	$.get("../../category/mens_submenu.html", function(data) {
+		$("#mens_submenu_xs").html(data);
+	});
+}); 
+
+// Mens thumbnais
+
+$(document).ready(function() {
+	$.get("../../category/mens/mens_tops_thumbnails.html", function(data) {
+		$("#mens_tops_thumbnails").html(data);
 	});
 }); 
 $(document).ready(function() {
-	$.get("../../category/boys/boys_accessories_thumbnails.html", function(data) {
-		$("#boys_accessories_thumbnails").html(data);
+	$.get("../../category/mens/mens_outerwear_thumbnails.html", function(data) {
+		$("#mens_outerwear_thumbnails").html(data);
 	});
 }); 
-
-
-
-
+$(document).ready(function() {
+	$.get("../../category/mens/mens_suits_thumbnails.html", function(data) {
+		$("#mens_suits_thumbnails").html(data);
+	});
+}); 
+$(document).ready(function() {
+	$.get("../../category/mens/mens_accessories_thumbnails.html", function(data) {
+		$("#mens_accessories_thumbnails").html(data);
+	});
+}); 
